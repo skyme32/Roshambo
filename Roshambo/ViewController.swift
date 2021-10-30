fd3b838f10ea4daf3759339502942322b5176ad4
Marcos <mmejiasg@zoho.com>
2021-10-30 16:04:19 +0200

    The init and finish commit.
    The init and finish commit.

--- Roshambo/ViewController.swift
+++ Roshambo/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    // MARK: ROCK
+    @IBAction private func playRock(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "play", sender: sender)
     }
 
+    // MARK: PAPER
+    @IBAction private func playPaper(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "play", sender: sender)
+    }
+
+    // MARK: Segue
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "play" {
+            let vc = segue.destination as! GameController
+            vc.userChoice = getUserShape(sender as! UIButton)
+        }
+    }
+
+    // MARK: Utilities
+    // The enum "Shape" represents a play or move
+    private func getUserShape(_ sender: UIButton) -> Shape {
+        // Titles are set to one of: Rock, Paper, or Scissors
+        let shape = sender.accessibilityHint!
+        return Shape(rawValue: shape)!
+    }
 
 }
 
